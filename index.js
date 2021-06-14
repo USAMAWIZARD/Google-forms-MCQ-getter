adb82a2e95cb06d87752b7cfe352996d5e1266ea
USAMAWIZARD <usamawizard@gmail.com>
2021-06-14 15:38:15 +0530

    linux drivers added

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ server=app();
 const spawn = require("child_process").spawn;
 server.get("/:examLink",(req,res)=>{
 const pythonProcess = spawn('python',["./ScrapQuestions.py",req.params.examLink]);
-console.log("sdf")
+
 pythonProcess.stdout.on('data', (data) => {
-console.log(data)
 res.json(`${data}`)
 res.end()
 });

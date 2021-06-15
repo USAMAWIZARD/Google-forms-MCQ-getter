ca8cfaef24382c2adfe15073b7ee45b54be9b038
Ubuntu <ubuntu@ip-172-31-45-155.ap-south-1.compute.internal>
2021-06-15 05:41:46 +0000

    selenium removed

--- ScrapQuestions.py
+++ ScrapQuestions.py
@@ -1,11 +1,12 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
-import pandas as pd
-QuesitonAndOptions={} #{"question":{"option1","option2","option3","optin4}}
-driver = webdriver.Chrome("chromedriver.exe")
-driver.get("https://docs.google.com/forms/d/e/1FAIpQLSeXCQSemX_8SCeMm6aqL-9KdotN22oDkeKKJ0RnLilCia9ofg/viewform")
 
-content = driver.page_source
+from bs4 import BeautifulSoup
+import  sys
+import requests
+import json
+QuesitonAndOptions={} #{"question":{"option1","option2","option3","optin4}}
+
+page =requests.get(sys.argv[1])
+content = page.content
 soup = BeautifulSoup(content,"html.parser")
 
 allQuestionsDiv=soup.findAll('div', attrs={'class':'freebirdFormviewerComponentsQuestionBaseRoot'})
@@ -16,4 +17,4 @@ for questionDiv in allQuestionsDiv:
 	optionsSpan=questionDiv.findAll('span',attrs={'class':'docssharedWizToggleLabeledLabelText'})
 	options = [i.text  for i in optionsSpan]
 	QuesitonAndOptions[question.text]=options
-print(QuesitonAndOptions)
+sys.stdout.write(json.dumps(QuesitonAndOptions))
